07546ae25211207805ea2f909add9a75910ed691
celerno <celerno@gmail.com>
2019-01-21 04:19:16 -0500

    aprendiendo a hacer api calls

--- www/views.py
+++ www/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+import requests
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hola, gueguegue')
 
 def estado(request):
-    return HttpResponse('todo fine: ' + str(datetime.now()))
+    req = requests.get('https://api.github.com/repos/celerno/fuente0/commits')
+    jData = json.loads(req.content)
+    print(jData[0]['sha'])
+    req = requests.get('https://api.github.com/repos/celerno/fuente0/commits/' + jData[0]['sha'])
+    jData = json.loads(req.content)
+    print(jData)
+    message = jData['commit']
+    return HttpResponse('última actualización: ' + message['message'] + ' / ' + message['committer']['date'])
+
