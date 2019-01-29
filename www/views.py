a1d272dc80d1deb71fe344146dfc4f9a8868cf27
celerno <celerno@gmail.com>
2019-01-29 01:02:04 -0700

    parser and db

--- www/views.py
+++ www/views.py
@@ -25,26 +25,54 @@ def estado(request):
     return HttpResponse('última actualización: ' + message['message'] + ' / ' + message['committer']['date'])
 
 cache = defaultdict()
-
+__client = MongoClient("mongodb://45.33.32.87:27017")
 def refresh(request):
-   
+    src = 'https://lopezobrador.org.mx/secciones/version-estenografica/'
     respuestas = []
-    text = ''
+    _db = __client["eventos"]
+    _eventos = _db["eventos_raw"]
+    page = 2
+
     if 'text' in cache:
         text = cache['text']
         print('using cache')
     else:
-        req = requests.get('https://lopezobrador.org.mx/secciones/version-estenografica/')
+        req = requests.get(src)
         text = req.text
-        cache['text'] = text
+        while req.status_code != 404:
+            print(str(datetime.now()) +  ' - GET ' + src + 'page/' + str(page))
+            req = requests.get(src + 'page/' + str(page))
+            page+=1
+            text+=req.text
+        cache['text']=text
 
-    veCodes = re.findall(r'VE-\d+-?\d*', text, re.RegexFlag.M)
-    veText = re.split(r'<p>\s*<?s?t?r?o?n?g?>?VE-\d+-?\d*', text)
+    __regex = r'(?:<p\s*(?:style=\"[^\"]+\")?>\s*(?:<strong>)?VE-\d+(?:[-.]\d+)?(?:</strong>)?\s*</p>|>VE-\d+(?:[-.]\d+)?\s*)'
+    veCodes = defaultdict()
+    for veCode in re.findall(__regex, text, re.RegexFlag.M):
+        veCode = veCode.replace('</','<').replace('<p>','').replace('<strong>','').replace('>','')
+        veCode = veCode[veCode.rindex('VE'):]
+        veCodes[veCode] = '<p><strong>' + veCode +'</strong></p>'
+    
+    
+    veText = re.split(__regex, text)
+    
+    response = str(len(veCodes.keys()))  + ' versiones estenográficas en ' + str(page - 1) + ' páginas<br><table>'
+    for i, ve in enumerate(veCodes.keys()):
+        veCodes[ve] += veText[i+1]
+        response += '<tr><td>' + str(i+1) + '</td><td>' + ve + '</td><td><textarea rows ="5" cols = "100">' + veCodes[ve] + '</textarea></td></tr>'
+        if _eventos.find({"veId":ve}).count() == 0:
+            _eventos.insert_one({"veId": ve, "text": veCodes[ve]})
+
+    response+='</table>'
+        
+
+
+    '''
     response ='<table><tr><td>code</td><td>preguntas</td><td>actores</td><td>nombres</td></tr>'
     for i, ve in enumerate(veText):
         entrevistados = {}
         preguntas = {}
-        ve = veCodes[i+1]
+        ve = veCodes[i] if len(veCodes) < i is not None else 'NULO'
         response+='<tr><td>'+ve +'</td><td>'
         text = veText[i]
         pq = PyQuery(text)
@@ -64,4 +92,5 @@ def refresh(request):
                         entrevistados[entrevistado] += 1
         response += str(len(preguntas))+'</td><td>' + str(len(entrevistados)) + '</td></td><td>' + str(entrevistados.keys()).replace('dict_keys([','').replace('\\n',' ').replace('\'','').replace('])','')+'</td></tr>'
     response+='</table>'
+    '''
     return HttpResponse(response)
